c9aa24565a214985075ae9817c64cb7a18d834c6
sharmin-alvandi <sha.alvandi1@gmail.com>
2023-09-12 16:14:25 -0400

    Prevent Zeitwerk::NameError

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,19 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
 # require "active_record/railtie"
 # require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
 # require "action_mailbox/engine"
 # require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -31,5 +31,8 @@ module ExchangeRateTool
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Set autoloader to classic to prevent Zeitwerk::NameError as Rails > 6 uses Zeitwerk gem as autoloader
+    config.autoloader = :classic
   end
 end
